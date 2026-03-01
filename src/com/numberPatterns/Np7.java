0bfab40fbe088c017d117a92e44cf65f98be3e40
Ajinkya10 <ajinkyalandge10@gmail.com>
2026-03-01 08:41:25 +0530

    update

---
+++ src/com/numberPatterns/Np7.java
@@ -0,0 +1,20 @@
+package com.numberPatterns;
+
+public class Np7 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+
+			int num = i % 2 == 0 ? 1 : 0;
+			System.out.print(num);
+
+			for (int j = 1; j < 5; j++) {
+				num = num == 1 ? 0 : 1;
+				System.out.print(num);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
