5b2ac2ce9544ab2547dcb489fe15e1e5ac8b5a10
Ajinkya10 <ajinkyalandge10@gmail.com>
2026-03-01 08:45:35 +0530

    update

---
+++ src/com/alphabetPatterns/Ap3.java
@@ -0,0 +1,17 @@
+package com.alphabetPatterns;
+
+public class Ap3 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(ch + " ");
+				ch++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
